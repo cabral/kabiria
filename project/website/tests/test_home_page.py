a17d6527554c5306f638729ccb995da03510bb75
Felipe Benites Cabral <felipe.benites@gmail.com>
2018-08-31 15:04:27 +0200

    Home page test refactor: Using TestCase

--- project/website/tests/test_home_page.py
+++ project/website/tests/test_home_page.py
@@ -1,48 +1,15 @@
-import socket
-
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import resolve
-
-from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from django.test import TestCase
 
 from project.website.views import index
 
 
-class BaseTestCase(StaticLiveServerTestCase):
-    """
-    Provides base test class which connects to the Docker
-    container running selenium.
-    """
-    host = '0.0.0.0' # Bind to 0.0.0.0 to allow external access
-
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.host = socket.gethostbyname(socket.gethostname())
-
-        cls.selenium = webdriver.Remote(
-            command_executor='http://selenium:4444/wd/hub',
-            desired_capabilities=DesiredCapabilities.CHROME,
-        )
-        cls.selenium.implicitly_wait(5)
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.selenium.quit()
-        super().tearDownClass()
-
-class HomePageTest(BaseTestCase):
+class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, index)
 
     def test_home_page_returns_correct_html(self):
-        self.live_server_url = self.live_server_url
-        self.selenium.get(self.live_server_url)
-
-        html = self.selenium.page_source
-        self.assertTrue(html.startswith('<!DOCTYPE html>'))
-        self.assertIn('<title>Kabiria</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
